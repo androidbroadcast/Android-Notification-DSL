9d41eb2721f418988c29c5349363b69940741739
Kirill Rozov <kirich1409@gmail.com>
2020-05-07 22:50:25 +0300

    Update sample

--- sample/src/main/java/com/kirich1409/androidnotificationdsl/sample/NotificationSample.kt
+++ sample/src/main/java/com/kirich1409/androidnotificationdsl/sample/NotificationSample.kt
@@ -1,9 +1,8 @@
 package com.kirich1409.androidnotificationdsl.sample
 
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
 import com.kirich1409.androidnotificationdsl.notification
+import com.kirich1409.androidnotificationdsl.utils.activityPendingIntent
 
 fun buildNotification(context: Context) =
     notification(context, NOTIFICATION_CHANNEL_DEFAULT, R.drawable.ic_android_white_24dp) {
@@ -11,22 +10,12 @@ fun buildNotification(context: Context) =
         actions {
             action(
                 title = "Edit",
-                intent = PendingIntent.getActivity(
-                    context,
-                    ACTION_EDIT_REQUEST_CODE,
-                    Intent(),
-                    PendingIntent.FLAG_CANCEL_CURRENT
-                ),
+                intent = context.activityPendingIntent(ACTION_EDIT_REQUEST_CODE, MainActivity::class),
                 icon = R.drawable.ic_baseline_edit_24
             )
             action(
                 title = "View",
-                intent = PendingIntent.getActivity(
-                    context,
-                    ACTION_VIEW_REQUEST_CODE,
-                    Intent(),
-                    PendingIntent.FLAG_CANCEL_CURRENT
-                ),
+                intent = context.activityPendingIntent<MainActivity>(ACTION_VIEW_REQUEST_CODE),
                 icon = R.drawable.ic_baseline_remove_red_eye_24
             )
         }
